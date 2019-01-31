ad93cd894c5649b6f59376254ada92c8ad9d50c8
Chris <justchris.vantine@gmail.com>
2019-01-31 01:11:39 -0500

    Added func for init spaceship by tonnage

--- shipyard/models/spacecraft.py
+++ shipyard/models/spacecraft.py
@@ -3,23 +3,31 @@ spacecraft.py
 
 Houses the spacecraft class
 """
+import json
+import os.path
+
 class Spacecraft:
     """
     The Spacecraft, primary class for organizing spaceship data
+
+    :param hull_tonnage: the size of the hull, in tons
     """
-    def __init__(self):
-        self.total_tonnage      = 0 # total tonnage of ship
+    def __init__(self, hull_tonnage):
+        self.tonnage            = 0 # total tonnage of ship
         self.hull_hp            = 0 # calculated as 1 per 50 tonnage
         self.structure_hp       = 0 # calculated as 1 per 50 tonnage
-        self.cargo              = 0 # amount of cargo space in tons
+        self.cargo              = 0 # amount of cargo space in tons (total tonnage - fuel - other modules)
         self.fuel_max           = 0 # amount of fuel tank
         self.fuel_jump          = 0 # amount of fuel required for a jump
         self.fuel_two_weeks     = 0 # amount of fuel required for 2 weeks of operation
-        self.cost               = 0 # current cost, updated on each edit
+        self.cost_hull          = 0 # cost of the hull alone
+        self.cost_total         = 0 # current cost, updated on each edit
+        self.hull_designation   = None   # A, B, C, etc.
         self.hull_type          = None   # steamlined, distributed, standard, etc.
         self.jdrive             = None   # jdrive object
         self.mdrive             = None   # mdrive object
         self.pplant             = None   # pplant object
+        self.armour             = list() # list of armor objects
         self.sensors            = list() # list of sensor objects
         self.turrets            = list() # list of turret objects
         self.bays               = list() # list of bay objects
@@ -28,3 +36,34 @@ class Spacecraft:
         self.drones             = list() # list of drone objects
         self.vehicles           = list() # list of vehicle objects
         self.additional_mods    = list() # list of strings describing misc features
+
+        # set the tonnage to that given at init
+        self.tonnage = hull_tonnage
+
+        # set cargo to maximum possible at first
+        self.cargo = hull_tonnage
+
+        # set hp based on tonnage
+        self.hull_hp = self.tonnage // 50
+        self.structure_hp = self.tonnage // 50
+
+        # pull hull cost from json, set that
+        data = None
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../resources/hull_data.json")
+        with open(path) as f:
+            data = json.load(f)
+
+        for key in data.keys():
+            # iterate through hull sizes for 
+            item_tonnage = data.get(key).get("tonnage")
+            if self.tonnage <= item_tonnage and (item_tonnage - self.tonnage) < 100:
+                self.hull_designation = key
+                self.cost_hull = data.get(key).get("cost")
+
+        if self.cost_hull == 0:
+            # picked an invalid hull size
+            return None
+        
+        # set the total cost to hull cost
+        self.cost_total = self.cost_hull
