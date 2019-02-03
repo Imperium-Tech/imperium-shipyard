c37d98dcf74698ad7576f0203030145687be15f7
Ryan Missel <rxm7244@rit.edu>
2019-02-03 00:39:49 -0500

    Added jdrive test

--- tests/test_jump_thrust.py
+++ tests/test_jump_thrust.py
@@ -22,3 +22,20 @@ def test_mdrive():
     spacecraft = Spacecraft(2000)
     spacecraft.add_mdrive("U")
     assert spacecraft.thrust == 4
+
+
+def test_jdrive():
+    # Testing valid drive to tonnage match
+    spacecraft = Spacecraft(200)
+    spacecraft.add_jdrive("A")
+    assert spacecraft.jump == 1
+
+    # Testing invalid drive to tonnage match
+    spacecraft = Spacecraft(100)
+    spacecraft.add_jdrive("J")
+    assert spacecraft.jump == 0
+
+    # Testing valid larger drive to tonnage match
+    spacecraft = Spacecraft(1600)
+    spacecraft.add_jdrive("Z")
+    assert spacecraft.jump == 5
