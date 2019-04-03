c8c2298b1982681d147d2a60e38e17726780d548
Ryan Missel <rxm7244@rit.edu>
2019-04-03 11:27:53 -0400

    Add jdrive GUI tests

--- tests/test_shipyard.py
+++ tests/test_shipyard.py
@@ -11,6 +11,7 @@ import sys
 
 
 def test_init():
+    # Test init of the window and its starting values
     app = QApplication(sys.argv)
 
     window = Window()
@@ -30,20 +31,123 @@ def test_init():
     assert window.cost_line_edit.text() == "0.0"
 
 
-def test_set_tonnage():
+def test_edit_tonnage():
+    # Test setting tonnage and checking GUI updates
     app = QApplication(sys.argv)
 
     window = Window()
-    tonnage = window.tonnage_line_edit
-    tonnage.setText("200")
+    window.tonnage_line_edit.setText("200")
     window.edit_tonnage()
     window.update_stats()
 
     assert window.spacecraft.tonnage == 200
     assert window.spacecraft.cargo == 200
     assert window.spacecraft.cost_total == 8.0
+    assert window.spacecraft.hull_hp == 4
+    assert window.spacecraft.structure_hp == 4
     assert window.tonnage_line_edit.text() == "200"
     assert window.cargo_line_edit.text() == "200"
     assert window.cost_line_edit.text() == "8.0"
     assert window.hull_hp_line_edit.text() == "4"
     assert window.structure_hp_line_edit.text() == "4"
+
+
+def test_edit_fuel():
+    # Test setting fuel and GUI updates
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.tonnage_line_edit.setText("100")
+    window.edit_tonnage()
+
+    window.fuel_line_edit.setText("50")
+    window.edit_fuel()
+    window.update_stats()
+
+    assert window.spacecraft.fuel_max == 50
+    assert window.spacecraft.cargo == 50
+    assert window.fuel_line_edit.text() == "50"
+    assert window.cargo_line_edit.text() == "50"
+
+
+def test_edit_jdrive_valid():
+    # Test valid jdrive edit
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.tonnage_line_edit.setText("100")
+    window.edit_tonnage()
+
+    window.jump_line_edit.setText("A")
+    window.edit_jdrive()
+    window.update_stats()
+
+    assert window.spacecraft.jdrive is not None
+    assert window.spacecraft.jump == 2
+    assert window.spacecraft.cargo == 90
+    assert window.spacecraft.cost_total == 12.0
+    assert window.jump_line_edit.text() == "2"
+    assert window.cargo_line_edit.text() == "90"
+    assert window.jump_label.text() == "A"
+    assert window.cost_line_edit.text() == "12.0"
+
+
+def test_edit_jdrive_invalid():
+    # Test invalid jdrive edit
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.tonnage_line_edit.setText("100")
+    window.edit_tonnage()
+
+    window.jump_line_edit.setText("AAB")
+    window.edit_jdrive()
+    window.update_stats()
+
+    assert window.spacecraft.jdrive is None
+    assert window.spacecraft.jump == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2, 0
+    assert window.jump_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.jump_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+
+    window.jump_line_edit.setText("I")
+    window.edit_jdrive()
+    window.update_stats()
+
+    assert window.spacecraft.jdrive is None
+    assert window.spacecraft.jump == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2,0
+    assert window.jump_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.jump_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+
+    window.jump_line_edit.setText("O")
+    window.edit_jdrive()
+    window.update_stats()
+
+    assert window.spacecraft.jdrive is None
+    assert window.spacecraft.jump == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2, 0
+    assert window.jump_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.jump_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+
+    window.jump_line_edit.setText("__")
+    window.edit_jdrive()
+    window.update_stats()
+
+    assert window.spacecraft.jdrive is None
+    assert window.spacecraft.jump == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2, 0
+    assert window.jump_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.jump_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
