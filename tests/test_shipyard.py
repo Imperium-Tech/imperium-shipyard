7b8f1540838ab0dfa195c07e0be3e620400e61b3
Ryan Missel <rxm7244@rit.edu>
2019-04-03 11:36:02 -0400

    Add mdrive GUI tests

--- tests/test_shipyard.py
+++ tests/test_shipyard.py
@@ -52,6 +52,9 @@ def test_edit_tonnage():
     assert window.structure_hp_line_edit.text() == "4"
 
 
+"""
+FUEL TESTS
+"""
 def test_edit_fuel():
     # Test setting fuel and GUI updates
     app = QApplication(sys.argv)
@@ -70,6 +73,9 @@ def test_edit_fuel():
     assert window.cargo_line_edit.text() == "50"
 
 
+"""
+JDRIVE TESTS
+"""
 def test_edit_jdrive_valid():
     # Test valid jdrive edit
     app = QApplication(sys.argv)
@@ -107,7 +113,7 @@ def test_edit_jdrive_invalid():
     assert window.spacecraft.jdrive is None
     assert window.spacecraft.jump == 0
     assert window.spacecraft.cargo == 100
-    assert window.spacecraft.cost_total == 2, 0
+    assert window.spacecraft.cost_total == 2.0
     assert window.jump_line_edit.text() == "0"
     assert window.cargo_line_edit.text() == "100"
     assert window.jump_label.text() == "-"
@@ -120,7 +126,7 @@ def test_edit_jdrive_invalid():
     assert window.spacecraft.jdrive is None
     assert window.spacecraft.jump == 0
     assert window.spacecraft.cargo == 100
-    assert window.spacecraft.cost_total == 2,0
+    assert window.spacecraft.cost_total == 2.0
     assert window.jump_line_edit.text() == "0"
     assert window.cargo_line_edit.text() == "100"
     assert window.jump_label.text() == "-"
@@ -133,7 +139,7 @@ def test_edit_jdrive_invalid():
     assert window.spacecraft.jdrive is None
     assert window.spacecraft.jump == 0
     assert window.spacecraft.cargo == 100
-    assert window.spacecraft.cost_total == 2, 0
+    assert window.spacecraft.cost_total == 2.0
     assert window.jump_line_edit.text() == "0"
     assert window.cargo_line_edit.text() == "100"
     assert window.jump_label.text() == "-"
@@ -146,8 +152,140 @@ def test_edit_jdrive_invalid():
     assert window.spacecraft.jdrive is None
     assert window.spacecraft.jump == 0
     assert window.spacecraft.cargo == 100
-    assert window.spacecraft.cost_total == 2, 0
+    assert window.spacecraft.cost_total == 2.0
     assert window.jump_line_edit.text() == "0"
     assert window.cargo_line_edit.text() == "100"
     assert window.jump_label.text() == "-"
     assert window.cost_line_edit.text() == "2.0"
+
+
+def test_jdrive_invalid_type():
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.tonnage_line_edit.setText("100")
+    window.edit_tonnage()
+
+    window.jump_line_edit.setText("Z")
+    window.edit_jdrive()
+    window.update_stats()
+
+    assert window.spacecraft.jdrive is None
+    assert window.spacecraft.jump == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2.0
+    assert window.jump_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.jump_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+    assert window.logger.text() == "Error: non-compatible drive to tonnage value - Drive Z to 100"
+
+
+"""
+MDRIVE TESTS
+"""
+def test_edit_mdrive_valid():
+    # Test valid mdrive edit
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.tonnage_line_edit.setText("100")
+    window.edit_tonnage()
+
+    window.thrust_line_edit.setText("A")
+    window.edit_mdrive()
+    window.update_stats()
+
+    assert window.spacecraft.mdrive is not None
+    assert window.spacecraft.thrust == 2
+    assert window.spacecraft.cargo == 98
+    assert window.spacecraft.cost_total == 6.0
+    assert window.thrust_line_edit.text() == "2"
+    assert window.cargo_line_edit.text() == "98"
+    assert window.thrust_label.text() == "A"
+    assert window.cost_line_edit.text() == "6.0"
+
+
+def test_edit_mdrive_invalid():
+    # Test invalid mdrive edits
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.tonnage_line_edit.setText("100")
+    window.edit_tonnage()
+
+    window.thrust_line_edit.setText("AAB")
+    window.edit_mdrive()
+    window.update_stats()
+
+    assert window.spacecraft.mdrive is None
+    assert window.spacecraft.thrust == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2.0
+    assert window.thrust_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.thrust_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+
+    window.thrust_line_edit.setText("I")
+    window.edit_mdrive()
+    window.update_stats()
+
+    assert window.spacecraft.mdrive is None
+    assert window.spacecraft.thrust == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2.0
+    assert window.thrust_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.thrust_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+
+    window.thrust_line_edit.setText("O")
+    window.edit_mdrive()
+    window.update_stats()
+
+    assert window.spacecraft.mdrive is None
+    assert window.spacecraft.thrust == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2.0
+    assert window.thrust_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.thrust_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+
+    window.thrust_line_edit.setText("___")
+    window.edit_mdrive()
+    window.update_stats()
+
+    assert window.spacecraft.mdrive is None
+    assert window.spacecraft.thrust == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2.0
+    assert window.thrust_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.thrust_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+
+
+def test_mdrive_invalid_type():
+    # Testing too high mdrive for tonnage
+    app = QApplication(sys.argv)
+
+    window = Window()
+    window.tonnage_line_edit.setText("100")
+    window.edit_tonnage()
+
+    window.thrust_line_edit.setText("Z")
+    window.edit_mdrive()
+    window.update_stats()
+
+    assert window.spacecraft.mdrive is None
+    assert window.spacecraft.thrust == 0
+    assert window.spacecraft.cargo == 100
+    assert window.spacecraft.cost_total == 2.0
+    assert window.thrust_line_edit.text() == "0"
+    assert window.cargo_line_edit.text() == "100"
+    assert window.thrust_label.text() == "-"
+    assert window.cost_line_edit.text() == "2.0"
+    assert window.logger.text() == "Error: non-compatible drive to tonnage value - Drive Z to 100"
+
