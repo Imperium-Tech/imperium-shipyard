fb4e1b4344638d3756f6a318bdb42833fd4cbda0
rxm7244 <rxm7244@rit.edu>
2020-02-10 11:41:14 -0500

    Add start to shipyard unit tests

---
+++ tests/test_shipyard.py
@@ -0,0 +1,63 @@
+"""
+@file test_shipyard
+
+Holds the unit tests for shipyard.py, which is mainly PyQT interactions
+"""
+import pytest
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import Qt
+from imperium.shipyard import Window
+import sys
+
+app = QApplication(sys.argv)
+
+
+@pytest.fixture()
+def window():
+    """ Before all for the tests, initializing a Window object """
+    window = Window()
+    yield window
+    window.close()
+
+
+def test_base_stats_init(window):
+    """ Tests for a base init of the Base Stats """
+    assert window is not None
+    assert window.spacecraft.tonnage == 100
+
+    # Base Stats
+    assert window.spacecraft.get_remaining_cargo() == 90
+    assert window.cargo_line_edit.text() == "90"
+
+    assert window.fuel_line_edit.text() == "0"
+    assert window.jump_line_edit.text() == "0"
+    assert window.jump_label.text() == "-"
+    assert window.thrust_line_edit.text() == "0"
+    assert window.thrust_label.text() == "-"
+
+    assert window.hull_hp_line_edit.text() == "2"
+    assert window.structure_hp_line_edit.text() == "2"
+    assert window.armour_line_edit.text() == "0"
+
+    assert window.discount.text() == "0"
+    assert window.cost_line_edit.text() == "2.500"
+
+
+def test_config_init(window):
+    """ Tests for base init of Config/Armor group """
+    assert window is not None
+
+    assert window.bridge_check.isChecked() is True
+    assert window.reflec_check.isChecked() is False
+    assert window.seal_check.isChecked() is False
+    assert window.stealth_check.isChecked() is False
+
+    assert window.meson_screen.isChecked() is False
+    assert window.nuclear_damper.isChecked() is False
+    assert window.fuel_scoop.isChecked() is False
+
+    assert window.hull_config_box.currentText() == "Standard"
+    assert window.sensors.currentText() == "Standard"
+    assert window.armor_combo_box.currentText() == "---"
+
